9d692901363213abb75997e7e6e836a83a33252e
AurimasPankunas <aurpan@ktu.lt>
2026-08-15 18:09:51 +0300

    Finished making BookBot, added a couple more books for testing, and a README for the project.

--- BookBot/main.py
+++ BookBot/main.py
@@ -1,17 +1,31 @@
 # This file contains the main logic of reading the contents of the book
-from stats import book_word_count, count_characters
+import sys
+from stats import book_word_count, chars_dict_to_sorted_list, count_characters
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+def print_report(filepath:str, num_words: int, sorted_characters: list[tuple[str,int]]):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for character in sorted_characters:
+        if character[0].isalpha():
+            print(f"{character[0]}: {character[1]}")
+    print("============= END ===============")
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     book_contents = get_book_text(filepath)
     num_words = book_word_count(book_contents)
-    print(f"Found {num_words} total words")
-    letters: dict[str, int] = count_characters(book_contents)
-    print(letters)
+    characters: dict[str, int] = count_characters(book_contents)
+    sorted_characters: list[tuple[str, int]] = chars_dict_to_sorted_list(characters)
+    print_report(filepath, num_words, sorted_characters)
 
 main()
--- BookBot/stats.py
+++ BookBot/stats.py
@@ -1,10 +1,12 @@
 # This file contains the logic of counting the words and syllables in a book
 
+# Returns how many words there are in the book
 def book_word_count(book: str) -> int:
     words = book.split()
 
     return len(words)
 
+# Returns a dict[str, int] which has every character and the amount each character appears
 def count_characters(book: str) -> dict[str,int]:
     characters: dict[str,int] = {}
     for word in book:
@@ -15,3 +17,18 @@ def count_characters(book: str) -> dict[str,int]:
                 continue
             characters[lower_character] += 1
     return characters
+
+# Returns the amount value of a single character in a book
+def sort_on(character: tuple[str,int]) -> int:
+    count_value = character[1]
+    return count_value
+
+# Turns the dictionary of characters into a list of tuples and sorts them from biggest to lowest
+def chars_dict_to_sorted_list(characters: dict[str,int]) -> list[tuple[str,int]]:
+    character_list: list[tuple[str, int]] = []
+
+    for character, value in characters.items():
+        character_list.append((character, value))
+
+    sorted_character_list = sorted(character_list, reverse=True, key=sort_on)
+    return sorted_character_list
